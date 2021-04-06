26df0e09aea6ff0ed2e0f2961d4e6a773336210b
yetm <yetingmao@163.com>
2021-04-07 07:40:39 +0800

    增加脚本

---
+++ cloudfunctions/script/index.js
@@ -0,0 +1,15 @@
+const cloud = require('wx-server-sdk');
+
+exports.main = async (event, context) => {
+  cloud.init({
+    env: "hongbao-1g1vxdep2e9a4001"
+  });
+  const db = cloud.database();
+  try {
+    return await db.collection('messages').where({
+      templateId: "WsI7r1v9d1hdKcWqT9cLmktb8CHGI8Rnfy4d6QlpGoc",
+    }).remove()
+  } catch (e) {
+    console.log(e)
+  }
+};
